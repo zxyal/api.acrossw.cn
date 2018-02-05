09d538965ed24069dbf276f58a22da3d95572150
zhaoxuyang <1170013162@qq.com>
2018-02-05 14:48:26 +0800

    更新用户流量

--- app/Console/Commands/UpdateUserPackage.php
+++ app/Console/Commands/UpdateUserPackage.php
@@ -6,6 +6,8 @@ use Illuminate\Console\Command;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\DB;
 
+use App\User;
+
 class UpdateUserPackage extends Command
 {
     /**
@@ -13,14 +15,14 @@ class UpdateUserPackage extends Command
      *
      * @var string
      */
-    protected $signature = 'update:package';
+    protected $signature = 'update:User transfer';
 
     /**
      * The console command description.
      *
      * @var string
      */
-    protected $description = 'Update User Package Data';
+    protected $description = '更新流量';
 
     /**
      * Create a new command instance.
@@ -39,42 +41,12 @@ class UpdateUserPackage extends Command
      */
     public function handle()
     {
-        $package = DB::table('package')->select('id', 'transfer', 'type')->where('status', 1)->get();
+        $num = User::update([
+            'u'               => 0,
+            'd'               => 0,
+            'transfer_enable' => 5000 * 1024 * 1024 * 1024
+        ]);
 
-        foreach ($package as $k => $v) {
-            $package_array[$v->id] = [
-                'transfer' => $v->transfer,
-                'type' => $v->type,
-            ];
-        }
-
-        $user_package = DB::table('user_package')->where('progress', 2)->get();
-
-        //当前月份
-        $now_months = date('m', time());
-
-        foreach ($user_package as $k => $v) {
-
-            $current_package = $package_array[$v->package_id];
-
-            if ($current_package['type'] == 1) {
-
-                if ($now_months != $v->last_update) {
-
-                    DB::table('user_package')->where('id', $v->id)->update([
-                        'last_update' => $now_months,
-                    ]);
-
-                    DB::table('user')->where('id', $v->user_id)->update([
-                        'u' => 0,
-                        'd' => 0,
-                        'transfer_enable' => $current_package['transfer'] * 1024 * 1024 * 1024
-                    ]);
-                }
-
-            }
-        }
-
-        Log::info('run time:' . date('Y-m-d H:i:s', time()));
+        echo '更新了'.$num;
     }
 }
